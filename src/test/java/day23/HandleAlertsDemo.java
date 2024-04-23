957d27b6bbfca16bc33582fc97ff195a54299409
nalboro <stev1989@gmail.com>
2024-04-23 10:24:38 +0200

    commit HandleAlertsDemo

---
+++ src/test/java/day23/HandleAlertsDemo.java
@@ -0,0 +1,36 @@
+package day23;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HandleAlertsDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+        driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+
+        //Alert alertWindow = driver.switchTo().alert();
+        Alert alertWindow = wait.until(ExpectedConditions.alertIsPresent()); //returns "driver.switchTo().alert();"
+
+        //get text from alert window
+        System.out.println(alertWindow.getText());
+
+        //confirm/click ok or cancel on alert window
+        //alertWindow.accept();
+        alertWindow.dismiss();
+
+
+    }
+}
+
