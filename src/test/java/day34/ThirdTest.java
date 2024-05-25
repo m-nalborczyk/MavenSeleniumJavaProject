2ff45ec33011dab497095ad177b34aeca5975f8b
nalboro <stev1989@gmail.com>
2024-05-25 14:52:27 +0200

    commit ThirdTest

---
+++ src/test/java/day34/ThirdTest.java
@@ -0,0 +1,27 @@
+package day34;
+
+import org.testng.annotations.Test;
+
+public class ThirdTest
+{
+  
+	@Test(priority=0)
+	void test1()
+	{
+		System.out.println("testing 1");
+	}
+	
+	@Test(priority=2)
+	void test2()
+	{
+		System.out.println("testing 2");
+		
+	}
+	
+	@Test(priority=3)
+	void test3()
+	{
+		System.out.println("testing 3");
+	}
+	
+}
