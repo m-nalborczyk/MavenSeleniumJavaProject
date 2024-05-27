db7a8bb2bcaa9b803972afaa5e8ffc85315f5b4d
nalboro <stev1989@gmail.com>
2024-05-27 19:29:41 +0200

    commit InvocationCountDemo.java to run multiple times the same test

---
+++ src/test/java/day37/InvocationCountDemo.java
@@ -0,0 +1,13 @@
+package day37;
+
+import org.testng.annotations.Test;
+
+public class InvocationCountDemo {
+
+	@Test(invocationCount=10)
+	void test()
+	{
+	System.out.println("testing...");	
+	}
+	
+}
